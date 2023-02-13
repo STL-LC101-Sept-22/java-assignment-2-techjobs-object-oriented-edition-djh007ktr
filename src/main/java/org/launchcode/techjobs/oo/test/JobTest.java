499a599769dce544523d0f93e858ce9e43e6de12
djh007ktr <djhill0531@gmail.com>
2023-02-12 19:03:48 -0600

    Task Four Tests Created&Passed

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by LaunchCode
@@ -9,4 +13,40 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        assertNotEquals(job1, job2);
+    }
+
+    @Test
+    public void testJobConstructorSetsALlFields() {
+        Job job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                   new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        //Check class assignments
+        assertTrue(job3 instanceof Job);
+        assertTrue(job3.getEmployer() instanceof Employer);
+        assertTrue(job3.getLocation() instanceof Location);
+        assertTrue(job3.getPositionType() instanceof PositionType);
+        assertTrue(job3.getCoreCompetency() instanceof CoreCompetency);
+        //Check value assignments
+        assertEquals(job3.getName(), "Product tester");
+        assertEquals(job3.getEmployer().getValue(), "ACME");
+        assertEquals(job3.getLocation().getValue(), "Desert");
+        assertEquals(job3.getPositionType().getValue(), "Quality control");
+        assertEquals(job3.getCoreCompetency().getValue(), "Persistence");
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job4 = new Job("Product designer", new Employer("General Electric"), new Location("San Francisco"),
+                   new PositionType("Engineering"), new CoreCompetency("Innovation"));
+        Job job5 = new Job("Product designer", new Employer("General Electric"), new Location("San Francisco"),
+                   new PositionType("Engineering"), new CoreCompetency("Innovation"));
+
+        assertFalse(job4.equals(job5));
+    }
 }
